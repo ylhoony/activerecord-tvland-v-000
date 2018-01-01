47a717694c578474dccc1ddfd8923c71fbffd34a
Hoon Lee <yl.hoony@gmail.com>
2018-01-01 12:21:42 +0000

    automatically backed up by learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -3,7 +3,7 @@ class Character < ActiveRecord::Base
   belongs_to :show
 
   def say_that_thing_you_say
-    "#{name} always says: #{catchphrase}"
+    "#{self.name} always says: #{self.catchphrase}"
   end
 
 end
